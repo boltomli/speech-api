a2b9a42118aec32979d2b051aadd52a9e55ce689
Song Li <songl@outlook.com>
2018-12-22 11:14:09 +0800

    Save settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { IonicStorageModule } from '@ionic/storage';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -21,7 +23,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-settings',
@@ -7,9 +8,25 @@ import { NavController } from 'ionic-angular';
 })
 export class SettingsPage {
   region: string;
+  key: string;
 
-  constructor(public navCtrl: NavController) {
-    this.region = 'westus';
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, private storage: Storage) {
+    this.storage.get('region').then((val) => {
+      this.region = val ? val : 'westus';
+    })
+    this.storage.get('key').then((val) => {
+      this.key = val;
+    })
   }
 
+  saveSettings(key: string, region: string) {
+    this.storage.set('key', key);
+    this.storage.set('region', region);
+
+    let toast = this.toastCtrl.create({
+      message: 'Settings saved',
+      duration: 1000
+    });
+    toast.present(toast);
+  }
 }
